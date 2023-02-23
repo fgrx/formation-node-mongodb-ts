95ff4f4ba2921a115e89b47a733e5d11b037437e
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-02-23 13:42:37 +0100

    Express installation and initialization

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello tout le monde !");
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Serveur lancé et à l'écoute sur le port ${port}`);
+});
