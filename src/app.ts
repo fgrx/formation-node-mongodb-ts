a9b70a5c36d92a86c07bfcbf97137bb0f911be2e
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-02-23 13:50:05 +0100

    router initialization and first route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import express, { Router } from "express";
 
 const app = express();
 
 const port = 3000;
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send(
+    `<html>
+        <head>
+            <title>Ma première application</title>
+        </head>
+        <body>
+        <h1>Hello world !</h1>
+        </body>
+    </html>`
+  );
+});
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Serveur lancé et à l'écoute sur le port ${port}`);
